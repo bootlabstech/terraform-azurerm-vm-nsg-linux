41b4ef83ddaa096eed7cd197036a4ff8df0405a5
Naveen-manokaran <naveen.manokaran@bootlabstech.com>
2022-09-19 14:43:47 +0530

    rewrite the module

--- main.tf
+++ main.tf
@@ -1,119 +1,65 @@
-terraform {
-  experiments = [module_variable_optional_attrs]
-}
-
-resource "azurerm_subnet" "subnet" {
-  for_each = { for vm in var.vms_configuration : vm.name => vm }
-  name                 = "sailor-subnet-01"
-  resource_group_name  = var.resource_group_name
-  virtual_network_name = each.value.vnet_name
-  address_prefixes     = each.value.subnet_id
-}
-
-resource "azurerm_network_interface" "vm_nic" { 
-  for_each = { for vm in var.vms_configuration : vm.name => vm }
-  name                = "${each.key}-${random_string.server_suffix[each.key].id}-nic"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-
-  ip_configuration {
-    name                          = "ipconfiguration1"
-    subnet_id                     =  azurerm_subnet.subnet[each.key].id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = each.value.public_ip_enabled ? azurerm_public_ip.vm_public_ip[each.key].id : null
-  }
-
-  lifecycle {
-    ignore_changes = [
-      # Ignore changes to tags, e.g. because a management agent
-      # updates these based on some ruleset managed elsewhere.
-      tags,
-    ]
-  }
-}
-
-resource "random_string" "server_suffix" {
-  for_each = { for vm in var.vms_configuration : vm.name => vm }
-  length           = 8
-  special          = false
-  upper            = false 
-  lower            = true
-  number           = true
-}
-
-resource "azurerm_public_ip" "vm_public_ip" {
-  for_each = { 
-    for vm in var.vms_configuration : vm.name => vm
-    if vm.public_ip_enabled
-  }
-  name                = "${each.key}-${random_string.server_suffix[each.key].id}-public-ip"
-  resource_group_name = var.resource_group_name
-  location            = var.location
-  allocation_method   = "Static"
-  domain_name_label   = "${each.key}-${random_string.server_suffix[each.key].id}"
-
-  lifecycle {
-    ignore_changes = [
-      # Ignore changes to tags, e.g. because a management agent
-      # updates these based on some ruleset managed elsewhere.
-      tags,
-    ]
-  }
-}
-
-resource "azurerm_virtual_machine" "vm" {
-  for_each = { for vm in var.vms_configuration : vm.name => vm }
-  name                  = "${each.key}-${random_string.server_suffix[each.key].id}-vm"
-  location              = var.location
-  resource_group_name   = var.resource_group_name
-  network_interface_ids = [azurerm_network_interface.vm_nic[each.key].id]
-  vm_size               = each.value.vm_size
+resource "azurerm_virtual_machine" "virtual_machine" {
+  name                             = var.name
+  location                         = var.location
+  resource_group_name              = var.resource_group_name
+  network_interface_ids            = [azurerm_network_interface.network_interface.id]
+  vm_size                          = var.vm_size
+  delete_os_disk_on_termination    = var.delete_os_disk_on_termination
+  delete_data_disks_on_termination = var.delete_data_disks_on_termination
 
   storage_image_reference {
-    publisher = each.value.image_publisher
-    offer     = each.value.image_offer
-    sku       = each.value.image_sku
-    version   = each.value.image_version
+    publisher = var.publisher
+    offer     = var.offer
+    sku       = var.sku
+    version   = var.storage_image_version
   }
 
   storage_os_disk {
-    name                      = "${each.key}-${random_string.server_suffix[each.key].id}-os-disk"
-    caching                   = each.value.storage_os_disk.caching                  
-    create_option             = each.value.storage_os_disk.create_option            
-    disk_size_gb              = each.value.storage_os_disk.disk_size_gb             
-    image_uri                 = each.value.storage_os_disk.image_uri                
-    os_type                   = each.value.storage_os_disk.os_type                  
-    write_accelerator_enabled = each.value.storage_os_disk.write_accelerator_enabled
-    managed_disk_id           = each.value.storage_os_disk.managed_disk_id          
-    managed_disk_type         = each.value.storage_os_disk.managed_disk_type        
-    vhd_uri                   = each.value.storage_os_disk.vhd_uri                  
+    name              = "${var.name}-disk"
+    caching           = var.caching
+    create_option     = var.create_option
+    managed_disk_type = var.managed_disk_type
+    os_type           = var.os_type
   }
 
   os_profile {
-    computer_name  = "${each.key}-${random_string.server_suffix[each.key].id}"
-    admin_username = each.value.os_username
-    admin_password = each.value.os_password
+    computer_name  = var.name
+    admin_username = var.admin_username
+    admin_password = var.admin_password
+    custom_data    = var.custom_data
   }
 
- dynamic os_profile_linux_config {
-    for_each = each.value.os_type == "Linux" ? [1] : []
+  dynamic "os_profile_linux_config" {
+    for_each = var.os_type == "Linux" ? [1] : []
     content {
-      disable_password_authentication = false
+      disable_password_authentication = var.disable_password_authentication
     }
   }
 
- dynamic os_profile_windows_config {
-    for_each = each.value.os_type == "Windows" ? [1] : []
+  dynamic "os_profile_windows_config" {
+    for_each = var.os_type == "Windows" ? [1] : []
     content {
-      provision_vm_agent = false
+      timezone = var.timezone
+      #provision_vm_agent = var.provision_vm_agent
     }
   }
-  
+
   lifecycle {
     ignore_changes = [
-      # Ignore changes to tags, e.g. because a management agent
-      # updates these based on some ruleset managed elsewhere.
       tags,
     ]
+  }
+  depends_on = [
+    azurerm_network_interface.network_interface
+  ]
+}
+resource "azurerm_network_interface" "network_interface" {
+  name                = var.network_interface_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  ip_configuration {
+    name                          = var.ip_name
+    subnet_id                     = var.subnet_id
+    private_ip_address_allocation = var.private_ip_address_allocation
   }
 }
--- variables.tf
+++ variables.tf
@@ -1,38 +1,135 @@
-variable resource_group_name{
-    type = string
-    description = "name of the resource group"
+# virtual_machine
+variable "name" {
+  type        = string
+  description = "Specifies the name of the Virtual Machine. Changing this forces a new resource to be created."
 }
 
-variable location{
-    type = string
-    description = "location of the resource group"
+variable "resource_group_name" {
+  type        = string
+  description = "name of the resource group"
 }
 
-variable vms_configuration {
-  description = "the main input variable which has all the configuration regarding the vm to be created"
-  type        = list(object({
-    name               = string,
-    public_ip_enabled  = bool,
-    vnet_name          = string,
-    subnet_id          = list(string),
-    vm_size            = string,
-    storage_os_disk    = object({
-      create_option             = string,
-      caching                   = optional(string),
-      disk_size_gb              = optional(number),
-      image_uri                 = optional(string), 
-      os_type                   = optional(string),
-      write_accelerator_enabled = optional(string),
-      managed_disk_id           = optional(string),
-      managed_disk_type         = optional(string),
-      vhd_uri                   = optional(string),
-    }),
-    image_publisher    = string,
-    image_offer        = string,
-    image_sku          = string,
-    image_version      = string,
-    os_username        = string,
-    os_password        = string,
-    os_type            = string,
-  }))
+variable "location" {
+  type        = string
+  description = "location of the resource group"
+}
+
+
+
+variable "vm_size" {
+  type        = string
+  description = "Specifies the size of the Virtual Machine. See also Azure VM Naming Conventions."
+}
+
+# storage_image_reference
+variable "publisher" {
+  type        = string
+  description = " Specifies the publisher of the image used to create the virtual machine. Examples: Canonical, MicrosoftWindowsServer"
+}
+
+variable "offer" {
+  type        = string
+  description = "Specifies the offer of the image used to create the virtual machine. Examples: UbuntuServer, WindowsServer"
+}
+
+variable "sku" {
+  type        = string
+  description = "Specifies the SKU of the image used to create the virtual machine. Examples: 18.04-LTS, 2019-Datacenter"
+}
+
+variable "storage_image_version" {
+  type        = string
+  description = "Specifies the version of the image used to create the virtual machine. Changing this forces a new resource to be created."
+  default     = "latest"
+}
+
+
+# storage_os_disk
+variable "caching" {
+  type        = string
+  description = "Specifies the caching requirements for the Data Disk. Possible values include None, ReadOnly and ReadWrite."
+  default     = "ReadWrite"
+}
+
+variable "create_option" {
+  type        = string
+  description = "Specifies how the data disk should be created. Possible values are Attach, FromImage and Empty."
+  default     = "FromImage"
+}
+
+variable "managed_disk_type" {
+  type        = string
+  description = "Specifies the type of managed disk to create. Possible values are either Standard_LRS, StandardSSD_LRS, Premium_LRS or UltraSSD_LRS."
+}
+
+variable "os_type" {
+  type        = string
+  description = "Specifies the Operating System on the OS Disk. Possible values are Linux and Windows."
+}
+
+# os_profile
+variable "admin_username" {
+  type        = string
+  description = "Specifies the name of the local administrator account."
+}
+
+variable "admin_password" {
+  type        = string
+  description = "The password associated with the local administrator account."
+}
+
+variable "custom_data" {
+  type        = string
+  description = "Specifies custom data to supply to the machine. On Linux-based systems, this can be used as a cloud-init script."
+}
+# network_interface
+variable "network_interface_name" {
+  type        = string
+  description = "The name of the Network Interface. Changing this forces a new resource to be created."
+}
+
+variable "ip_name" {
+  type        = string
+  description = "A name used for this IP Configuration."
+  default     = "internal"
+}
+
+variable "subnet_id" {
+  type        = string
+  description = "The ID of the Subnet where this Network Interface should be located in."
+}
+
+variable "private_ip_address_allocation" {
+  type        = string
+  description = "The allocation method used for the Private IP Address. Possible values are Dynamic and Static"
+}
+
+variable "disable_password_authentication" {
+  type        = bool
+  description = "Specifies whether password authentication should be disabled. If set to false, an admin_password must be specified."
+  default     = false
+}
+
+variable "timezone" {
+  type        = string
+  description = "(optional) describe your variable"
+  default     = "India Standard Time"
+}
+
+# variable "provision_vm_agent" {
+#   type        = bool
+#   description = "Should the Azure Virtual Machine Guest Agent be installed on this Virtual Machine? Defaults to false"
+#   default     = true
+# }
+
+variable "delete_os_disk_on_termination" {
+  type        = bool
+  description = " Should the OS Disk (either the Managed Disk / VHD Blob) be deleted when the Virtual Machine is destroyed? Defaults to false."
+  default     = true
+}
+
+variable "delete_data_disks_on_termination" {
+  type        = bool
+  description = "Should the Data Disks (either the Managed Disks / VHD Blobs) be deleted when the Virtual Machine is destroyed? Defaults to false."
+  default     = true
 }
